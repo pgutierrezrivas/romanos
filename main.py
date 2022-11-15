9cb00c31e3ab0bd13a603562940470367502d68e
Patricia Gutierrez <patriciagr@gmx.es>
2022-11-15 19:42:37 +0100

    primer commit

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ f)4.5 -> RomanNumberError("Debe ser un entero")
 class RomanNumberError(Exception):
     pass
 
+
+'''
 diccionario = {
     'M':1000,
     'D':500,
@@ -25,6 +27,11 @@ diccionario = {
     'I':1
 }
 
+for c,v in diccionario.items():
+    print(c+"-"+str(v))
+
+'''
+
 unidades = {
     'I':1, 'II':2, 'III':3,
     'IV':4, 'V':5, 'VI':6,
@@ -32,7 +39,7 @@ unidades = {
 }
 
 decenas = {
-    'X':10, 'XX':20, 'XXX':3
+    'X':10, 'XX':20, 'XXX':30,
     'XL':40, 'L':50, 'LX':60,
     'LXX':70, 'LXXX':80, 'XC':90
 }
@@ -43,5 +50,6 @@ centenas = {
     'DCC':700,'DCCC':800,'CM':900
 }
 
-for c,v in diccionario.items():
-    print(c+"-"+str(v))
+miles = {
+    'M': 1000, 'MM':2000, 'MMM':3000
+}
