6e5e841ec29761f98541a812509189ee99aea375
Patricia Gutierrez <patriciagr@gmx.es>
2022-11-17 19:08:10 +0100

    kata dia 17-11-22

--- main.py
+++ main.py
@@ -16,26 +16,26 @@ class RomanNumberError(Exception):
     pass
 
 
-u = {
-    'I':1, 'II':2, 'III':3,
-    'IV':4, 'V':5, 'VI':6,
-    'VII':7, 'VIII':8, 'IX':9
+unidades = {
+    1:'I', 2:'II', 3:'III',
+    4:'IV', 5:'V', 6:'VI',
+    7:'VII', 8:'VIII', 9:'IX'
 }
 
-d = {
-    'X':10, 'XX':20, 'XXX':30,
-    'XL':40, 'L':50, 'LX':60,
-    'LXX':70, 'LXXX':80, 'XC':90
+decenas = {
+    10:'X', 20:'XX', 30:'XXX',
+    40:'XL', 50:'L', 60:'LX',
+    70:'LXX', 80:'LXXX', 90:'XC'
 }
 
-c = {
-    'C':100,'CC':200,'CCC':300,
-    'CD':400,'D':500,'DC':600,
-    'DCC':700,'DCCC':800,'CM':900
+centenas = {
+    100:'C', 200:'CC', 300:'CCC',
+    400:'CD', 500:'D', 600:'DC',
+    700:'DCC', 800:'DCCC', 900:'CM'
 }
 
-m = {
-    'M': 1000, 'MM':2000, 'MMM':3000
+miles = {
+    1000:'M', 2000:'MM', 3000:'MMM'
 }
 
 def entero_a_romano(num):
