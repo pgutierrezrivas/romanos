1dec6cef85aff0b9bd3c8d7f4a960d302c7436e8
Patricia Gutierrez <patriciagr@gmx.es>
2022-11-17 20:54:54 +0100

    mejora funcion enteero_a_romano

--- main.py
+++ main.py
@@ -15,46 +15,33 @@ f)4.5 -> RomanNumberError("Debe ser un entero")
 class RomanNumberError(Exception):
     pass
 
-
-unidades = {
+dic_entero_a_romano = {
     1:'I', 2:'II', 3:'III',
     4:'IV', 5:'V', 6:'VI',
-    7:'VII', 8:'VIII', 9:'IX'
-}
-
-decenas = {
+    7:'VII', 8:'VIII', 9:'IX',
     10:'X', 20:'XX', 30:'XXX',
     40:'XL', 50:'L', 60:'LX',
-    70:'LXX', 80:'LXXX', 90:'XC'
-}
-
-centenas = {
+    70:'LXX', 80:'LXXX', 90:'XC',
     100:'C', 200:'CC', 300:'CCC',
     400:'CD', 500:'D', 600:'DC',
-    700:'DCC', 800:'DCCC', 900:'CM'
-}
-
-miles = {
+    700:'DCC', 800:'DCCC', 900:'CM',
     1000:'M', 2000:'MM', 3000:'MMM'
 }
 
-def entero_a_romano(num):
-    lista = []
-    num = str(num)
+def entero_a_romano(num:int) -> str:
+    
+    num = "{:0>4d}".format(num)
+    lista = list(num)
+    longitud = len(lista) 
+    numero_romano = ""
+    
 
-    if len(num)<4:
-       num = "{:0>4s}".format(num)
-    # for digito in num:
-    #        lista.append(digito)
-    lista = list(num) 
-    for i in range(len(lista)):
-        lista[i] = lista[i]+"0"*((len(lista)-1 )-i)
-    print(lista)
+    for i in range(longitud):
+        longitud -=1
+        lista[i] += "0"*longitud
+        numero_romano += dic_entero_a_romano.get(int(lista[i]),'')
 
-    return lista
-
-
-
+    return numero_romano
 
 print("funcion en accion",entero_a_romano(336))
 
